f8c2fe12c85e9aa37cc9c1cdf8ee6d0b726db969
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-06-27 18:08:31 -0300

    Add claim tokens function

--- src/token/main.mo
+++ src/token/main.mo
@@ -1,5 +1,6 @@
 import Principal "mo:base/Principal";
 import HashMap "mo:base/HashMap";
+import Debug "mo:base/Debug";
 
 actor Token {
     
@@ -26,5 +27,18 @@ actor Token {
     public query func getSymbol() : async Text {
         return symbol;
     };
+
+    public shared(msg) func payOut() : async Text {
+        if(balances.get(msg.caller) == null) {
+            let amount = 10000;
+            balances.put(msg.caller, amount);
+            return "Success";
+        } else {
+            return "Already claimed";
+        }
+        
+    };
+
+
 
 };
--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import {token} from "../../../declarations/token";
 
 function Faucet() {
 
-  async function handleClick(event) {
+  const [isDisable, setDisable] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
 
+  async function handleClick(event) {
+    setDisable(true);
+    const result = await token.payOut();
+    setText(result);
   }
 
   return (
@@ -14,10 +20,14 @@ function Faucet() {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 10,000 DANG coins to your account.</label>
+      <label>Get your free DAngela tokens here! Claim 10,000 DIPO tokens to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button 
+        id="btn-payout" 
+        onClick={handleClick}
+        disabled={isDisable}
+        >
+          {buttonText}
         </button>
       </p>
     </div>
