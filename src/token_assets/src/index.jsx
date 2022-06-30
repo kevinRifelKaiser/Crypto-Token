fdab63d99da3affa8ca4e7765ec2a6af123198fb
Kevin Rifel Kaiser <90354193+kevinRifelKaiser@users.noreply.github.com>
2022-06-30 19:28:07 -0300

    Update index.jsx

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -2,10 +2,6 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
 
-const init = async () => { 
   ReactDOM.render(<App />, document.getElementById("root"));
-}
-
-init();
 
 
